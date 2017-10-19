fb14b94780da07b23d42f1c754387eacf0c6fa8a
hawmember <k.brandao-graca@gmx.de>
2017-10-19 14:26:18 +0200

    Head erstellung
    Anpassungen in der CSS und in der main.js für den header

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-
+$( document ).ready(function() {
+	// Wechsel von z-index
+	$(".signIn-button").click(function () {
+		$('.sign').css({height:'363'});
+		$('.signIn').addClass('active-panel');
+		$('.signUp').removeClass('active-panel');
+	});
+	$(".signUp-button").click(function () {
+		$('.sign').css({height:'435'});
+		$('.signUp').addClass('active-panel');
+		$('.signIn').removeClass('active-panel');
+	});
+});
