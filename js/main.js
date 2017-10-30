15f9f261f964962388e13803e08f8fc76f0a0d5a
hawmember <k.brandao-graca@gmx.de>
2017-10-30 15:43:18 +0100

    Test

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 $( document ).ready(function() {
 	// Wechsel von z-index
 	$(".signIn-button").click(function () {
-		$('.sign').css({height:'363'});
 		$('.signIn').addClass('active-panel');
 		$('.signUp').removeClass('active-panel');
 	});
 	$(".signUp-button").click(function () {
-		$('.sign').css({height:'435'});
 		$('.signUp').addClass('active-panel');
 		$('.signIn').removeClass('active-panel');
 	});
