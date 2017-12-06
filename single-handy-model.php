47d1a78f1c2ef7c81a3a9893d3eb10a59899aacc
hawmember <k.brandao-graca@gmx.de>
2017-12-06 11:34:34 +0100

    Änderungen

---
+++ single-handy-model.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html class="no-js" lang="DE">
+    <head>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Smartphone Portal</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon.png">
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+        <script src="js/ityped.js"></script>
+    </head>
+    <body>
+        <!-- Handy Model --><!-- Samsung Galaxy S8 plus -->
+        <div class="handy-model" handy="samsungGalaxyS8Plus">
+            <!-- Handy Bild -->
+            <div class="handy-img">
+                <img src="img/_handybilder/samsungGalaxyS8Plus.png" />
+            </div>
+            <!-- Handy Infos -->
+            <div class="handy-info">
+                <h4>Samsung Galaxy S8+ Plus</h4>
+                <p class="handy-release">18/April/2017</p>
+                <ul>
+                    <li class="her"><span>Hersteller:</span> <span>Samsung</span></li>
+                    <li class="spe"><span>Speicher:</span> <span>64gb</span></li>
+                    <li class="far"><span>Farbe:</span> <span>Orchid Grey, Midnight Black, Arctic Silver</span></li>
+                    <li class="pre"><span>Preis:</span> <span>899€</span></li>
+                    <li class="abm"><span>Abmessung:</span> <span>159,5mm/73,4mm/8,1mm</span></li>
+                    <li class="gew"><span>Gewicht:</span> <span>173g</span></li>
+                    <li class="displayMain openMore">
+                        <a>Display</a>
+                        <ul>
+                            <li class="gro"><span>Display Größe:</span> <span>6,2"</span></li>
+                            <li class="auf"><span>Display Auflösung:</span> <span>1440x2960 Pixel</span></li>
+                            <li class="art"><span>Display Art:</span> <span>Super-AMOLED</span></li>
+                        </ul>
+                    </li>
+
+                </ul>
+            </div>
+            <!-- Mehr Erfahren -->
+            <div class="handy-more">
+                <div class="circle open">
+                    <img src="img/arrow.png" />
+                </div>
+            </div>
+        </div><!-- Hand Model Ende --><!-- Samsung Galaxy S8 plus -->
+
+        <!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>-->
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <!-- Einfügen der JS datei -->
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
+
+
+
+
+
+
+
